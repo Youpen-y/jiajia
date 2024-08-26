ca28ddc247dafcc66d0902c77fc41b1c8fa54eb3
Youpen-y <1586590320@qq.com>
2024-08-26 15:11:35 +0800

    Add two debug points

--- apps/sor/src/sor.c
+++ apps/sor/src/sor.c
@@ -179,8 +179,9 @@ int main(int argc, char *argv[])
   */	
   jia_config(WVEC,ON);
   jia_config(ADWD,ON);
-
+  printf("88888888888888\n");
   slave();
+  printf("99999999999999\n");
   jia_exit();
 }
 
