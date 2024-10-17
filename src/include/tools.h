54f984344cf017da6c916abfaf2b8de4384852ee
Youpen-y <1586590320@qq.com>
2024-10-17 16:44:40 +0800

    Remove old newmsg declaration

--- src/include/tools.h
+++ src/include/tools.h
@@ -11,7 +11,7 @@ void inittools();
 void assert0(int, char *, ...);
 void assert(int cond, char *format, ...);
 void jiaexitserver(jia_msg_t *req);
-jia_msg_t *newmsg();
+unsigned char *newmsg();
 void freemsg(jia_msg_t *msg);
 void appendmsg(jia_msg_t *msg, unsigned char *str, int len);
 void printmsg(jia_msg_t *msg, int right);
