f7e364fe2d0f8009ddabb04502855cf7fee27eda
Youpen-y <yongy2022@outlook.com>
2024-12-24 10:26:39 +0800

    Scripts killpid.sh improvement

--- killpid.sh
+++ killpid.sh
@@ -2,20 +2,20 @@
 # shellcheck disable=SC2086
 
 programs=("ep" "hello" "is" "lu" "mm" "pi" "sor" "tsp" "water")
-servers=("cpuserver2" "cpuserver3" "cpuserver4" "cpuserver5" "cpuserver6")
-#servers=("yyp196" "yyp192")
+#servers=("cpuserver2" "cpuserver3" "cpuserver4" "cpuserver5" "cpuserver6")
+servers=("yyp192")
 
 for server in "${servers[@]}"; do
 	if [ -n "$1" ]; then
 		# get pid
-		pid=$(ssh $server "cd jianode/$program && lsof -w $program" | awk 'NR==2 {print $2}')
+		pid=$(ssh $server "cd jianode/$1 && lsof -w $1" | awk 'NR==2 {print $2}')
 	
 		# pid non-null
 		if [ -n "$pid" ]; then
-		echo "process $pid in running on $server"
+			echo "process $pid in running on $server"
 			ssh $server "kill -9 $pid && echo \"process $pid has been killed\""
 		fi
-		echo "$program on $server is ok"
+		echo "$1 on $server is ok"
 	else
 		for pg in "${programs[@]}"; do
 					
