05dc4e3f4953c2e4019be03de8126aff97c7111e
segzix <shengzixuan2003@outlook.com>
2024-12-30 16:23:52 +0800

    add comments for rdma_comm.c

--- src/rdma/rdma_comm.c
+++ src/rdma/rdma_comm.c
@@ -25,12 +25,12 @@ jia_dest_t dest_info[Maxhosts];
 int arg[Maxhosts];
 
 void *tcp_server_thread(void *arg) {
+    /* step 1: create server tcp socket fd (setting reuse) */
     int server_id = *(int *)arg;
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) {
         log_err("Failed to create server socket");
     }
-
     int opt = 1;
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
                    sizeof(opt)) < 0) {
@@ -38,27 +38,26 @@ void *tcp_server_thread(void *arg) {
         exit(EXIT_FAILURE);
     }
 
+    /* step 2: bind fd and listen to addr::port (ANY_ADDR) */
     struct sockaddr_in server_addr = {0};
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr =
         inet_addr((char *)system_setting.hosts[system_setting.jia_pid].ip);
     server_addr.sin_port = htons(ctx.tcp_port + server_id);
-
     if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) <
         0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-
     if (listen(server_fd, 1) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
+    log_out(3, "Server listening on <%s, %d>...\n",
+             system_setting.hosts[system_setting.jia_pid].ip,
+             ctx.tcp_port + server_id);
 
-    printf("Server listening on <%s, %d>...\n",
-           system_setting.hosts[system_setting.jia_pid].ip,
-           ctx.tcp_port + server_id);
-
+    /* step 3: accept ANY_ADDR to set new fd */
     struct sockaddr_in client_addr = {0};
     socklen_t addrlen = sizeof(client_addr);
     int new_socket =
@@ -67,9 +66,9 @@ void *tcp_server_thread(void *arg) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
+    log_out(3, "Server: connection established with client.\n");
 
-    printf("Server: connection established with client.\n");
-
+    /* step 4: test connection */
     bool flag = true;
     while (1) {
         ssize_t bytes_read =
@@ -82,73 +81,75 @@ void *tcp_server_thread(void *arg) {
             send(new_socket, &flag, sizeof(bool), 0);
         }
     }
-
-    printf("[%d]\t0x%04x\t0x%06x\t0x%06x\t%016llx:%016llx\n", server_id,
+    log_out(3, "[%d]\t0x%04x\t0x%06x\t0x%06x\t%016llx:%016llx\n", server_id,
            dest_info[server_id].lid, dest_info[server_id].qpn,
            dest_info[server_id].psn,
            dest_info[server_id].gid.global.subnet_prefix,
            dest_info[server_id].gid.global.interface_id);
 
+    /* step 5: close fd */
     close(new_socket);
     close(server_fd);
     return NULL;
 }
 
 void *tcp_client_thread(void *arg) {
+    /* step 1: create client tcp socket fd (setting timeout) */
     int client_id = *(int *)arg;
     int client_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (client_fd < 0) {
         log_err("Failed to create client socket");
     }
-
-    struct sockaddr_in server_addr = {0};
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(system_setting.hosts[client_id].ip);
-    server_addr.sin_port = htons(ctx.tcp_port + system_setting.jia_pid);
-
     struct timeval timeout;
-    timeout.tv_sec = 2;  // 超时秒数
-    timeout.tv_usec = 0; // 微秒部分
-
+    timeout.tv_sec = 2;
+    timeout.tv_usec = 0;
     if (setsockopt(client_fd, SOL_SOCKET, SO_SNDTIMEO, (const char *)&timeout,
                    sizeof(timeout)) < 0) {
         perror("setsockopt failed");
         close(client_fd);
     }
 
+    /* step 2: connect server_addr */
+    struct sockaddr_in server_addr = {0};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(system_setting.hosts[client_id].ip);
+    server_addr.sin_port = htons(ctx.tcp_port + system_setting.jia_pid);
     while (connect(client_fd, (struct sockaddr *)&server_addr,
                    sizeof(server_addr)) < 0) {
         if (errno == EINPROGRESS || errno == EWOULDBLOCK) {
-            printf("Connection timed out\n");
+            log_err("Connection timed out\n");
         }
     }
 
+    /* step 3: test connection */
     bool flag = false;
     while (1) {
         send(client_fd, &dest_info[system_setting.jia_pid], sizeof(jia_dest_t),
              0);
         if (recv(client_fd, &flag, sizeof(bool), 0) == 1 && flag) {
-            printf("server returned %s\n", flag == true ? "true" : "false");
+            log_out(3, "server returned %s\n", flag == true ? "true" : "false");
             break;
         }
     }
 
+    /* step 4: close fd */
     close(client_fd);
     return NULL;
 }
 
 void exchange_dest_info() {
+    /* step 1: init thread args */
     for (int i = 0; i < Maxhosts; i++) {
         arg[i] = i;
     }
 
+    /* step 2: create server and client threads */
     for (int i = 0; i < system_setting.hostc; i++) {
         if (i == system_setting.jia_pid) {
             continue;
         }
         pthread_create(&ctx.server_tid[i], NULL, tcp_server_thread, &arg[i]);
     }
-
     for (int i = 0; i < system_setting.hostc; i++) {
         if (i == system_setting.jia_pid) {
             continue;
@@ -156,6 +157,7 @@ void exchange_dest_info() {
         pthread_create(&ctx.client_tid[i], NULL, tcp_client_thread, &arg[i]);
     }
 
+    /* step 3: wait server and client threads */
     for (int i = 0; i < system_setting.hostc; i++) {
         if (i == system_setting.jia_pid) {
             continue;
@@ -198,6 +200,7 @@ void init_comm_qp_context(struct jia_context *ctx) {
 
     /* step 4: create QP && change QP's state to RTS */
     {
+        /* step 4.1: create qp with giving type and cap */
         struct ibv_qp_attr attr;
         struct ibv_qp_init_attr init_attr = {
             .qp_context = ctx->context,
@@ -217,17 +220,18 @@ void init_comm_qp_context(struct jia_context *ctx) {
         };
         ctx->qp = ibv_create_qp(ctx->pd, &init_attr);
 
+        /* step 4.2: query qp to get queue's info */
         ibv_query_qp(ctx->qp, &attr, IBV_QP_CAP, &init_attr);
         if (init_attr.cap.max_inline_data >= Msgsize) {
             // use inline data for little messages
             ctx->send_flags |= IBV_SEND_INLINE;
         }
 
+        /* step 4.3: change state from IBV_QPS_INIT to IBV_QPS_RTS */
         attr.qp_state = IBV_QPS_INIT;
         attr.pkey_index = 0;
         attr.port_num = ctx->ib_port;
         attr.qkey = 0x11111111;
-
         if (ibv_modify_qp(ctx->qp, &attr,
                           IBV_QP_STATE | IBV_QP_PKEY_INDEX | IBV_QP_PORT |
                               IBV_QP_QKEY)) {
@@ -246,8 +250,8 @@ void init_comm_qp_context(struct jia_context *ctx) {
         }
     }
 
+    /* step 5: update localhost's gid and other information in dest_info*/
     ibv_query_port(ctx->context, ctx->ib_port, &ctx->portinfo);
-
     dest_info[system_setting.jia_pid].lid = ctx->portinfo.lid;
     dest_info[system_setting.jia_pid].qpn = ctx->qp->qp_num;
     dest_info[system_setting.jia_pid].psn = 0;
@@ -258,11 +262,11 @@ void init_comm_qp_context(struct jia_context *ctx) {
         memset(&dest_info[system_setting.jia_pid].gid, 0,
                sizeof(dest_info[system_setting.jia_pid].gid));
     }
-
     dest_info[system_setting.jia_pid].gid.global.subnet_prefix =
         be64toh(dest_info[system_setting.jia_pid].gid.global.subnet_prefix);
     dest_info[system_setting.jia_pid].gid.global.interface_id =
         be64toh(dest_info[system_setting.jia_pid].gid.global.interface_id);
+
     log_info(3,
              "Local address: LID 0x%04x, QPN 0x%06x, PSN 0x%06x, GID "
              "%016llx:%016llx",
@@ -297,25 +301,25 @@ void init_comm_qp_context(struct jia_context *ctx) {
 }
 
 void init_ack_qp_context(struct jia_context *ctx) {
+    /* step 1: create completion channel */
     ctx->ack_send_channel = ibv_create_comp_channel(ctx->context);
     ctx->ack_recv_channel = ibv_create_comp_channel(ctx->context);
 
-    ctx->ack_send_cq =
-        ibv_create_cq(ctx->context, 1, NULL, ctx->ack_send_channel, 0);
-    ctx->ack_recv_cq =
-        ibv_create_cq(ctx->context, 1, NULL, ctx->ack_recv_channel, 0);
-
+    /* step 2: register memory region */
     ctx->ack_send_mr =
         ibv_reg_mr(ctx->pd, &ack_send, sizeof(int), IBV_ACCESS_LOCAL_WRITE);
     ctx->ack_recv_mr =
         ibv_reg_mr(ctx->pd, &ack_recv, sizeof(int), IBV_ACCESS_LOCAL_WRITE);
 
-    ctx->send_cq =
+    /* step 3: create completion queue */
+    ctx->ack_send_cq =
         ibv_create_cq(ctx->context, 1, NULL, ctx->ack_send_channel, 0);
-    ctx->recv_cq =
+    ctx->ack_recv_cq =
         ibv_create_cq(ctx->context, 1, NULL, ctx->ack_recv_channel, 0);
 
+    /* step 4: create QP && change QP's state to RTS */
     {
+        /* step 4.1: create qp with giving type and cap */
         struct ibv_qp_attr attr;
         struct ibv_qp_init_attr init_attr = {
             .qp_context = ctx->context,
@@ -333,14 +337,15 @@ void init_ack_qp_context(struct jia_context *ctx) {
             .qp_type = IBV_QPT_UD,
             .sq_sig_all = 1
         };
-
         ctx->ack_qp = ibv_create_qp(ctx->pd, &init_attr);
 
+        // TODO: should choose immediate state?
+
+        /* step 4.2: change state from IBV_QPS_INIT to IBV_QPS_RTS */
         attr.qp_state = IBV_QPS_INIT;
         attr.pkey_index = 0;
         attr.port_num = ctx->ib_port;
         attr.qkey = 0x11111111;
-
         if (ibv_modify_qp(ctx->qp, &attr,
                           IBV_QP_STATE | IBV_QP_PKEY_INDEX | IBV_QP_PORT |
                               IBV_QP_QKEY)) {
@@ -359,6 +364,7 @@ void init_ack_qp_context(struct jia_context *ctx) {
         }
     }
 
+    /* step 5: get ack_qpn */
     dest_info[system_setting.jia_pid].ack_qpn = ctx->ack_qp->qp_num;
 }
 
