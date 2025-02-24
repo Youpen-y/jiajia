1f67cbb518ad6e29cf35c68795dd51d4f2dc4c4d
segzix <zixuansheng14@gmail.com>
2025-02-24 18:56:36 +0800

    test

--- src/rdma/rdma_client.c
+++ src/rdma/rdma_client.c
@@ -25,32 +25,64 @@ void *rdma_client_thread(void *arg) {
         /* step 0: get sem value to print */
         int semvalue;
         sem_getvalue(&ctx.outqueue->busy_count, &semvalue);
-        log_info(4, "pre client outqueue dequeue busy_count value: %d", semvalue);
+        log_info(4, "pre client outqueue dequeue busy_count value: %d",
+                 semvalue);
         // wait for busy slot
         sem_wait(&ctx.outqueue->busy_count);
         sem_getvalue(&ctx.outqueue->busy_count, &semvalue);
-        log_info(4, "enter client outqueue dequeue! busy_count value: %d", semvalue);
+        log_info(4, "enter client outqueue dequeue! busy_count value: %d",
+                 semvalue);
 
         /* step 1: give seqno */
         msg_ptr = (jia_msg_t *)(ctx.outqueue->queue[ctx.outqueue->head]);
         msg_ptr->seqno = snd_seq[msg_ptr->topid];
 
         /* step 2: post send mr */
-        while (post_send(&ctx.connect_array[msg_ptr->topid])) {
-            log_info(3, "msg <from:%d, to:%d, seq:%d, data:%s> send failed\n", msg_ptr->frompid,
-                    msg_ptr->topid, msg_ptr->seqno, msg_ptr->data);
-        }
+        if (msg_ptr->topid == system_setting.jia_pid) {
 
+            /* step 1: memcpy outqueue to inqueue */
+            msg_queue_t *inqueue = ctx.connect_array[msg_ptr->topid].inqueue;
+            memcpy(inqueue->queue[inqueue->tail], msg_ptr, sizeof(jia_msg_t));
+
+            log_info(3, "pre inqueue [tail]: %d, [busy_value]: %d [post_value]: %d", inqueue->tail,
+                     atomic_load(&inqueue->busy_value), atomic_load(&inqueue->post_value));
+
+            /* step 2: sub free_value and add busy_value */
+            if (atomic_load(&(inqueue->free_value)) <= 0) {
+                log_err("busy value error <= 0");
+            } else {
+                atomic_fetch_sub(&(inqueue->free_value), 1);
+            }
+            atomic_fetch_add(&(inqueue->busy_value), 1);
+
+            /* step 3: update tail point */
+            pthread_mutex_lock(&inqueue->tail_lock);
+            inqueue->tail = (inqueue->tail + 1) % QueueSize;
+            pthread_mutex_unlock(&inqueue->tail_lock);
+
+            log_info(3, "after inqueue [tail]: %d, [busy_value]: %d [post_value]: %d",
+                     inqueue->tail, atomic_load(&inqueue->busy_value), atomic_load(&inqueue->post_value));
+
+        } else {
+            while (post_send(&ctx.connect_array[msg_ptr->topid])) {
+                log_info(3,
+                         "msg <from:%d, to:%d, seq:%d, data:%s> send failed\n",
+                         msg_ptr->frompid, msg_ptr->topid, msg_ptr->seqno,
+                         msg_ptr->data);
+            }
+        }
 #ifdef DOSTAT
         if (statflag == 1) {
             jiastat.msgsndcnt++;
             jiastat.msgsndbytes +=
-                (((jia_msg_t *)outqueue.queue[outqueue.head])->size + Msgheadsize);
+                (((jia_msg_t *)outqueue.queue[outqueue.head])->size +
+                 Msgheadsize);
         }
 #endif
 
-        log_info(3, "msg <from:%d, to:%d, seq:%d, data:%s> send successfully\n", msg_ptr->frompid,
-                msg_ptr->topid, msg_ptr->seqno, msg_ptr->data);
+        log_info(3, "msg <from:%d, to:%d, seq:%d, data:%s> send successfully\n",
+                 msg_ptr->frompid, msg_ptr->topid, msg_ptr->seqno,
+                 msg_ptr->data);
 
         /* step 3: update snd_seq and head ptr */
         snd_seq[msg_ptr->topid]++;
@@ -59,7 +91,8 @@ void *rdma_client_thread(void *arg) {
         /* step 4: sem post and print value */
         sem_post(&ctx.outqueue->free_count);
         sem_getvalue(&ctx.outqueue->free_count, &semvalue);
-        log_info(4, "after client outqueue dequeue free_count value: %d", semvalue);
+        log_info(4, "after client outqueue dequeue free_count value: %d",
+                 semvalue);
     }
 }
 
@@ -139,80 +172,4 @@ int post_send() {
 		       number of WC elements */);
 
     return 0;
-}
-
-void *rdma_client_thread(void *arg) {
-    while (1) {
-        /* step 0: get sem value to print */
-        int semvalue;
-        sem_getvalue(&ctx.outqueue->busy_count, &semvalue);
-        log_info(4, "pre client outqueue dequeue busy_count value: %d",
-                 semvalue);
-        // wait for busy slot
-        sem_wait(&ctx.outqueue->busy_count);
-        sem_getvalue(&ctx.outqueue->busy_count, &semvalue);
-        log_info(4, "enter client outqueue dequeue! busy_count value: %d",
-                 semvalue);
-
-        /* step 1: give seqno */
-        msg_ptr = (jia_msg_t *)(ctx.outqueue->queue[ctx.outqueue->head]);
-        msg_ptr->seqno = snd_seq[msg_ptr->topid];
-
-        /* step 2: post send mr */
-        if (msg_ptr->topid == system_setting.jia_pid) {
-
-            /* step 1: memcpy outqueue to inqueue */
-            msg_queue_t *inqueue = ctx.connect_array[msg_ptr->topid].inqueue;
-            memcpy(inqueue->queue[inqueue->tail], msg_ptr, sizeof(jia_msg_t));
-
-            log_info(3, "pre inqueue [tail]: %d, [busy_value]: %d [post_value]: %d", inqueue->tail,
-                     atomic_load(&inqueue->busy_value), atomic_load(&inqueue->post_value));
-
-            /* step 2: sub free_value and add busy_value */
-            if (atomic_load(&(inqueue->free_value)) <= 0) {
-                log_err("busy value error <= 0");
-            } else {
-                atomic_fetch_sub(&(inqueue->free_value), 1);
-            }
-            atomic_fetch_add(&(inqueue->busy_value), 1);
-
-            /* step 3: update tail point */
-            pthread_mutex_lock(&inqueue->tail_lock);
-            inqueue->tail = (inqueue->tail + 1) % QueueSize;
-            pthread_mutex_unlock(&inqueue->tail_lock);
-
-            log_info(3, "after inqueue [tail]: %d, [busy_value]: %d [post_value]: %d",
-                     inqueue->tail, atomic_load(&inqueue->busy_value), atomic_load(&inqueue->post_value));
-
-        } else {
-            while (post_send(&ctx.connect_array[msg_ptr->topid])) {
-                log_info(3,
-                         "msg <from:%d, to:%d, seq:%d, data:%s> send failed\n",
-                         msg_ptr->frompid, msg_ptr->topid, msg_ptr->seqno,
-                         msg_ptr->data);
-            }
-        }
-#ifdef DOSTAT
-        if (statflag == 1) {
-            jiastat.msgsndcnt++;
-            jiastat.msgsndbytes +=
-                (((jia_msg_t *)outqueue.queue[outqueue.head])->size +
-                 Msgheadsize);
-        }
-#endif
-
-        log_info(3, "msg <from:%d, to:%d, seq:%d, data:%s> send successfully\n",
-                 msg_ptr->frompid, msg_ptr->topid, msg_ptr->seqno,
-                 msg_ptr->data);
-
-        /* step 3: update snd_seq and head ptr */
-        snd_seq[msg_ptr->topid]++;
-        ctx.outqueue->head = (ctx.outqueue->head + 1) % QueueSize;
-
-        /* step 4: sem post and print value */
-        sem_post(&ctx.outqueue->free_count);
-        sem_getvalue(&ctx.outqueue->free_count, &semvalue);
-        log_info(4, "after client outqueue dequeue free_count value: %d",
-                 semvalue);
-    }
 }
